5808aaa7c066e5b9b5f3a3f87282939d2f024ad4
ejones <ejonesnospam@gmail.com>
2020-01-25 16:50:55 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,35 +1,3 @@
-"""The ssh integration."""
-
-import logging
-
-import voluptuous as vol
-
-from homeassistant.helpers import discovery
-from homeassistant.const import CONF_DEVICE
-import homeassistant.helpers.config_validation as cv
-
-_LOGGER = logging.getLogger(__name__)
-
-DOMAIN = "ssh"
-
-SSH_PLATFORMS = ["switch", "sensor"]
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {vol.Optional(CONF_DEVICE, default=DEFAULT_DEVICE): cv.string}
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
 
 
-def setup(hass, config):
-    """Set up the ssh switch platform."""
 
-    for platform in SSH_PLATFORMS:
-        discovery.load_platform(
-            hass, platform, DOMAIN, {"device": config[DOMAIN][CONF_DEVICE]}, config
-        )
-
-    return True
